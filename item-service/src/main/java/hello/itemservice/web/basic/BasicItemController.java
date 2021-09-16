e0c793e25c1e9776061a72925fbf534ffdccb8a1
DolphaGo <adamdoha@naver.com>
2021-09-17 00:53:11 +0900

    상품 수정

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -85,6 +85,26 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        final Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    /**
+     * HTML 폼 전송은 PUT / PATCH를 지원하지 않는다. GET/POST만 사용할 수 있다.
+     * PUT/PATCH는 HTTP API 전송시에 사용한다.
+     * 스프링에서 HTTP POST 요청할 때 히든 필드를 통해서 PUT, PATCH 매핑을 사용하는 방법이 있지만, HTTP 요청상 POST 요청이다.
+     *
+     */
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}"; // redirect로 이동시키면 url 자체가 변하게 된다. 여기서 {itemId}는 pathVariable을 그대로 가져온다.
+    }
+
     /**
      * 테스트용 데이터 추가
      */
