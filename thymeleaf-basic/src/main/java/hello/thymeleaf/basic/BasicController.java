3dbb6ed82394a40e1b84ff44db1920f3952d490b
DolphaGo <adamdoha@naver.com>
2021-09-26 23:45:31 +0900

    기본 객체들

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -53,16 +56,29 @@ public class BasicController {
         list.add(userA);
         list.add(userB);
 
-        final Map<String,User> map = new HashMap<>();
+        final Map<String, User> map = new HashMap<>();
 
         map.put("userA", userA);
         map.put("userB", userB);
 
-        model.addAttribute("user",userA);
+        model.addAttribute("user", userA);
         model.addAttribute("users", list);
         model.addAttribute("userMap", map);
 
         return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
     }
 
     @Data
