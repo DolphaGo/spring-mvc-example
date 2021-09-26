03114bf236a947255c3194ac352384522699c908
DolphaGo <adamdoha@naver.com>
2021-09-26 23:37:53 +0900

    변수 - SpringEL

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,24 +1,31 @@
 package hello.thymeleaf.basic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
 
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
 
     @GetMapping("/text-basic")
-    public String textBasic(Model model){
-        model.addAttribute("data","<b>Hello Spring!</b>");
+    public String textBasic(Model model) {
+        model.addAttribute("data", "<b>Hello Spring!</b>");
         // <b>Hello Spring!</b> 라고 작성하게 되면, 렌더링이 안됨, 렌더링을 해보면 &lt;b&gt;Hello Spring!&lt;/b&gt; 와 같은 문자로 됨
         return "basic/text-basic";
     }
 
     @GetMapping("/text-unescaped")
-    public String textUnEscaped(Model model){
-        model.addAttribute("data","<b>Hello Spring!</b>");
+    public String textUnEscaped(Model model) {
+        model.addAttribute("data", "<b>Hello Spring!</b>");
         // <b>Hello Spring!</b> 라고 작성하게 되면, 렌더링이 안됨, 렌더링을 해보면 &lt;b&gt;Hello Spring!&lt;/b&gt; 와 같은 문자로 됨
         // 이러한 특수 문자를 HTML Entity라고 합니다. 웹 브라우저는 `<` 를 태그의 시작으로 인식합니다.
         // HTML에서 사용하는 특수 문자를 HTML 엔티티 언어로 변환하는 것을 HTML Escape라고 합니다.
@@ -36,4 +43,36 @@ public class BasicController {
      * 사용상에 자유로움을 주는데, 이걸 다 unescaped 처리하면 화면이 다 깨지게 된다 (실전 경험)
      * 따라서, 기본적으로 escaped 처리가 되어 있는 것이다.
      */
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        final User userA = new User("userA", 10);
+        final User userB = new User("userB", 20);
+
+        final List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        final Map<String,User> map = new HashMap<>();
+
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user",userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(final String username, final int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
 }
