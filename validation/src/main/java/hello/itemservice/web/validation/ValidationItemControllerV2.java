9e8d1d10a0de0782703c2e2ce4da8639bbb9f672
DolphaGo <adamdoha@naver.com>
2021-10-27 02:18:02 +0900

    오류 코드와 메세지 처리1

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -68,7 +68,7 @@ public class ValidationItemControllerV2 {
      * 예를 들어서 @ModelAttribute Item item 에 대해서 item에 대해 검증하고 싶다면 바로 다음에 BindResult가 와야만 한다.
      * BindResult는 Model에 자동으로 포함된다.
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         // 검증 로직
@@ -135,6 +135,50 @@ public class ValidationItemControllerV2 {
             final int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            // bindingResult는 자동으로 view에 담아갑니다. 그래서 따로 modelAttribute로 담지 않아도 됩니다.
+            // 스프링에서 BindingResult가 기본적으로 되어있다는 것이 그런 것들을 포함하고 있는 의미이기 때문입니다.
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[] { "required.item.itemName" }, null, "상품 이름은 필수입니다."));
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(
+                    new FieldError("item", "price", item.getPrice(), false, new String[] { "range.item.price" }, new Object[] { 1000, 1000000 }, "가격은 1,000 ~ 1,000,000 까지만 허용합니다."));
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() > 9999 || item.getQuantity() <= 0) {
+            bindingResult.addError(
+                    new FieldError("item", "quantity", item.getQuantity(), false, new String[] { "max.item.quantity" }, new Object[] { 9999 }, "수량은 1개이상, 최대 9,999까지 허용합니다."));
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            final int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(
+                        new ObjectError("item", new String[] { "totalPriceMin", "required.default" }, new Object[] { 10000, resultPrice }, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+                // new String[]인 이유는, 순서대로 우선순위를 갖고 있기 때문입니다.
+                // default message 조차 없으면 오류가 나게 됩니다.
             }
         }
 
