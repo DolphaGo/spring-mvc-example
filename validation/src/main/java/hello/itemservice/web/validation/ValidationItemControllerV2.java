acd1bfd2ed6733ccce39ee6b5cbf037c666e9e98
DolphaGo <adamdoha@naver.com>
2021-12-02 21:48:05 +0900

    오류 코드와 메시지 처리2

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -153,8 +153,11 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        log.info("objectName={}", bindingResult.getObjectName()); // objectName=item
+        log.info("target={}", bindingResult.getTarget()); // target=Item(id=null, itemName=, price=10, quantity=2)
 
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
@@ -176,9 +179,52 @@ public class ValidationItemControllerV2 {
             final int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.addError(
-                        new ObjectError("item", new String[] { "totalPriceMin", "required.default" }, new Object[] { 10000, resultPrice }, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+                        new ObjectError("item", new String[] { "totalPriceMin", "required.default" }, new Object[] { 10000, resultPrice },
+                                        "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
                 // new String[]인 이유는, 순서대로 우선순위를 갖고 있기 때문입니다.
                 // default message 조차 없으면 오류가 나게 됩니다.
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            // bindingResult는 자동으로 view에 담아갑니다. 그래서 따로 modelAttribute로 담지 않아도 됩니다.
+            // 스프링에서 BindingResult가 기본적으로 되어있다는 것이 그런 것들을 포함하고 있는 의미이기 때문입니다.
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        log.info("objectName={}", bindingResult.getObjectName()); // objectName=item
+        log.info("target={}", bindingResult.getTarget()); // target=Item(id=null, itemName=, price=10, quantity=2)
+
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName", "required");
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.rejectValue("price", "range", new Object[] { 1000, 10000 }, null);
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() > 9999 || item.getQuantity() <= 0) {
+            bindingResult.rejectValue("quantity", "max", new Object[] { 9999 }, null);
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            final int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[] { 10000, resultPrice }, null);
             }
         }
 
